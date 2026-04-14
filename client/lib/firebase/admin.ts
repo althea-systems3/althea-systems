9c658da1de12cb2df6f73c8740f60ece284d0b69
Killian <killian.doizelet@supdevinci-edu.fr>
2026-04-14 16:31:44 +0200

    fix du chatbot

--- client/lib/firebase/admin.ts
+++ client/lib/firebase/admin.ts
@@ -7,6 +7,12 @@ function getFirebaseApp(): admin.app.App {
     return cachedFirebaseApp;
   }
 
+  const existingApps = admin.apps;
+  if (existingApps.length > 0 && existingApps[0]) {
+    cachedFirebaseApp = existingApps[0];
+    return cachedFirebaseApp;
+  }
+
   const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
   const privateKey = process.env.FIREBASE_PRIVATE_KEY;
